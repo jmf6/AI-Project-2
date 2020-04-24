04cd768caab38cfb1e1672308626249617a1cac6
jmf6 <46510153+jmf6@users.noreply.github.com>
2020-04-24 17:55:07 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,26 +3,30 @@
 #include "game.hpp"
 #include "player.hpp"
 #include "Rules.hpp"
+#include "human.hpp"
 #include <iostream>
 
 using namespace std;
 
 int main(){
-    Game g = Game();
+    char choice = 'y';
+    do{
+        cout << "Welcome to UNO!" << endl;
+        Game g = Game(); //initialize game
 
-    for(size_t i = 0; i < g.p1.hand.size(); i++){
-        cout << g.p1.hand.at(i).getColor() << " " << g.p1.hand.at(i).getValue();
-        if(g.p1.hand.at(i).getIsAction() == true){
-            cout << " " << g.p1.hand.at(i).getAction() << endl;
-        }
-        else{
-            cout << endl;
-        }
-    }
+        cout << g.p1.getName() << endl;
+        cout << "top of discard pile: " << g.getDiscard().getColor() << " " << g.getDiscard().getValue() << endl;
+        cout << Card::turnOver.getValue() << endl;
+        cout << "Enter play sequence" << endl;
+        cout << g.p1.getNumCards() << endl;
+        g.p1.play();
+        cout << "Exiting play sequence" << endl;
+        //auto mrule = Rules::rules.at(0);
+        //cout << mrule.test() << endl;
 
-    cout << "top of discard pile: " << g.getDiscard().getColor() << " " << g.getDiscard().getValue() << endl;
-    cout << Card::turnOver.getValue() << endl;
-    auto mrule = Rules::rules.at(0);
-    cout << mrule.test() << endl;
+        choice = 'n';
+        cout << choice << endl;
+    }while(choice != 'n' && choice != 'N');
+
     return 0;
-}  
+}
