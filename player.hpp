e47a727bb6dcd8e797e97d2849132616423043bd
jmf6 <46510153+jmf6@users.noreply.github.com>
2020-04-24 17:55:54 -0500

    Update player.hpp

--- player.hpp
+++ player.hpp
@@ -4,20 +4,21 @@
 #include "card.hpp"
 #include <vector>
 #include <string>
+#include <iostream>
 
 class Player{                              // Base Player class
     private:                                // No one may access this other than the player
         int numCards;
-	std::string name;
+        std::string name;
     protected:                              // protected means inherited classes can access this data, as opposed to private
         void sName(std::string name);
     public:
         Player();
-        virtual int play();                 // override-able behavior
+        virtual Card play();                 // override-able behavior
         void addToHand(Card c);
-	void setNumCards();
+        void setNumCards();
         std::vector<Card> hand;
         int getNumCards();
-	std::string getName();
+        std::string getName();
 };
 #endif
