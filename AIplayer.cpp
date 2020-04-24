72d7637b4fdcae2705397aa9bcc296eff1740605
jmf6 <46510153+jmf6@users.noreply.github.com>
2020-04-24 17:52:26 -0500

    Update AIplayer.cpp

--- AIplayer.cpp
+++ AIplayer.cpp
@@ -4,8 +4,8 @@ AIplayer::AIplayer(){
     sName("CPU 1");
 }
 
-int AIplayer::play(){
+Card AIplayer::play(){
     //do AI shit here
 
-    return -1; //just until AI shit is done
+    return Card("none", -1); //just until AI shit is done
 }
