d0c5a277f2523eddf9456a3abb0e97f32f52fadb
jmf6 <46510153+jmf6@users.noreply.github.com>
2020-04-24 16:01:26 -0500

    Update AIplayer.hpp

--- AIplayer.hpp
+++ AIplayer.hpp
@@ -1,10 +1,12 @@
+#pragma once
 #include "player.hpp"
 #ifndef AIPLAYER_HPP
 #define AIPLAYER_HPP
 
 class AIplayer : public Player{
     public:
-	int play() override; //put all the AI magic here
+        AIplayer();
+        int play() override; //put all the AI magic here
 };
 
 #endif
